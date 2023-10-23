64f4a9ac17751da3650a4101008ab82a6e191005
ianco99 <iankuz25@gmail.com>
2023-10-23 13:07:04 -0300

    Added ids system made in class

---
+++ Desarrollo-3-2023/Assets/Code/Scripts/Ids/Id.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "_State", menuName = "ScriptableObject/Id")]
+public class Id : ScriptableObject
+{
+}

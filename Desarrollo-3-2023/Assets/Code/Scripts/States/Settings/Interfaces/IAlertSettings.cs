91d20ad02c49325ac92e5f6f7cc0588d9309b9ef
ianco99 <71154684+ianco99@users.noreply.github.com>
2023-11-15 01:04:32 -0300

    ok

--- Desarrollo-3-2023/Assets/Code/Scripts/States/Settings/Interfaces/IAlertSettings.cs
+++
@@ -1,6 +0,0 @@
-public interface IAlertSettings
-{
-    public float alertSpeed { get => alertSpeed; set => alertSpeed = value; }
-    public float alertAcceleration { get => alertAcceleration; set => alertAcceleration = value; }
-
-}
